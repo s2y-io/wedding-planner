21d2d8ed7436f4562b18d9906866839a267b9df4
SHAN ANUSHKA <60233383+ShanAnushka@users.noreply.github.com>
2021-04-27 01:35:35 +0530

    HeaderMenu
    Header Menu for all customer UIs

---
+++ headermenu.php
@@ -0,0 +1,54 @@
+<!-- header -->
+    <div id="header" class="header" style="position: fixed; top:0; width: 100%; display: block; transition: top 0.3s;">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-2 col-lg-2 col-md-12 col-sm-12 col-12">
+                    <!-- header-logo -->
+                    <div class="header-logo">
+                        <a href="index.php"><img style="float:left; " src="images/logo1.png" alt="Wedding Vendor & Supplier Directory HTML Template "></a>
+                    </div>
+                    <!-- /.header-logo -->
+                </div>
+                <div class="col-xl-10 col-lg-10 col-md-12 col-sm-12 col-12">
+                    <!-- navigations -->
+                     <div id="navigation">
+                        <ul>
+                            <li><a href="index.php">HOME</a>
+                            </li>
+                            <!-- <li><a href="about-us.php">ABOUT US</a> -->
+                            </li>
+                            <li><a href="#" title="#">DIRECTORY</a>
+                                <ul>
+                                    <li><a href="list-grid-view.php">VENDORS</a></li>
+                                    <li><a href="blog-listing.php" title="">EXHIBITIONS & EVENTS</a></li>
+
+                                </ul>
+                            </li>
+                            <li><a href="offers.php">OFFERS</a>
+                            </li>
+                            <li><a href="#">GALLERY</a>
+                                <ul>
+                                    <li><a href="real-wedding-list.php" title="">IMAGE GALLERY</a></li>
+                                    <li><a href="video-gallery.php" title="">VIDEO GALLERY</a></li>
+                                    <!--<li><a href="blog-single.php" title="">WEDDING PLANNING</a></li>-->
+                                </ul>
+                            </li>
+                            <li><a href="contact-us.php">CONTACT US</a></li>
+                            <li style="padding-right: 3px;"><a href="magazine.php" class="btn btn-default btn-xs" style="font-size: 10px;color: white; background-color: #3b5999;border-radius: 10px;">Magazine</a></li>
+                            <li style="padding-right: 3px;"><a href="marrage-proposal-view.php" class="btn btn-default btn-xs" style="font-size: 10px;color: white; background-color: #e4405f;border-radius: 10px;">Marrage Prposal</a></li>
+                            <li style="padding-right: 3px;"><a href="couple-form.php" class="btn btn-default btn-xs" style="font-size: 10px; background-color: #2c3e50; color: white; border-radius: 10px;">Customer</a></li>
+                            <li style="padding-right: 5px;"><a href="vendor-form.php" class="btn btn-default btn-xs" style="font-size: 10px; background-color: #16a085;color: white;border-radius: 10px;">Vendor</a></li>
+                        </ul>
+                    </div>
+                    <!-- /.navigations -->
+                </div>
+                <!--<div class="col-xl-3 col-lg-3 text-right col-md-3 col-sm-12 col-12 d-none d-xl-block d-lg-block">
+                    <div class="header-btn">
+                       <a href="couple-form.php" class="btn btn-default btn-xs" style="font-size: 12px; background-color: #FF945E;border-color: #FF945E; color: black;">Customer</a>
+                       <a href="vendor-form.php" class="btn btn-default btn-xs" style="font-size: 12px; background-color: #FF945E; border-color: #FF945E; color: black;">Vendor</a>
+                    </div>
+                </div>-->
+            </div>
+        </div>
+    </div>
+    <!-- /.header -->
