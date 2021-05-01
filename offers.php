4f0d05580f0c2a3c701d4646ddcb3aaa7f0c091b
Kithmini Oshadi <kithmini90@gmail.com>
2021-05-01 14:05:45 +0530

    Add dashboard offers

---
+++ offers.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Silver Star</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+    <!-- FontAwesome icon -->
+    <link href="fontawesome/css/fontawesome-all.css" rel="stylesheet">
+    <!-- Fontello icon -->
+    <link href="fontello/css/fontello.css" rel="stylesheet">
+    <!-- Favicon icon -->
+    <link rel="shortcut icon" type="image/X-UA-Compatible" href="images/logo1.png">
+    <!-- Style CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <!-- header-top -->
+    <div class="header-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-sm-6 col-md-6 col-sm-6 col-6 d-none d-xl-block d-lg-block d-md-block">
+                    <div class="header-text">
+                        <p class="wlecome-text">Welcome to Silver Star Wedding Directory.</p>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-sm-6 col-md-6 col-sm-12 col-12">
+                    <div class="header-text text-right">
+                        <p>Are You Vendor? <a href="vendor-form.html">List Your Business</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/.header-top -->
+    <!-- header -->
+    <?php include '../silverstar/headermenu.php';?>
+    <!-- /.header -->
+    <!-- page-header -->
+    <div class="offer-header">
+        <div class="container">
+            <div class="row">
+                <!-- page section -->
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div class="page-caption">
+                        <h1 class="page-title">Offers</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.page-header -->
+    <!-- vendor-section -->
+    <div class="content">
+        <div class="container">
+            <div class="row">
+
+            <?php
+include "../silverstar/connection/DB.php";
+$resultsetGetlist = mysqli_query($connection, "SELECT * FROM offerstbl WHERE delflag='0'");
+while ($row = mysqli_fetch_row($resultsetGetlist)) {
+    ?>
+                   <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
+                            <div class="post-block">
+                                <!-- post vertical block -->
+                                <div class="post-img zoomimg">
+                                    <a href="#" ><?php echo '<img src=../silverstar/admin/' . $row[7] . ' alt=""  class="img-fluid">'; ?></a>
+                                </div>
+                                <div class="post-content" style="background-color: white;">
+                                    <h2 class="post-heading"><a href="#" class="post-title"><?php echo $row[1]; ?></a></h2>
+                                    <p class="meta">
+                                        <span class="meta-posted-by">Call :<?php echo $row[5]; ?></span></br>
+                                        <span class="meta-date">Start : <?php echo $row[3]; ?></span><span class="meta-date">End : <?php echo $row[4]; ?></span>
+                                    </p>
+                                    <p><?php echo $row[2]; ?></p>
+                                </div>
+                            </div>
+                            <!-- /.post vertical block -->
+                        </div>  <?php }?>
+
+
+                <!-- sidebar-section -->
+
+                <!-- /.sidebar-section -->
+            </div>
+            <!-- paginations -->
+
+            <!-- /.paginations -->
+        </div>
+    </div>
+    <!-- social-media-section -->
+    <div class="social-media-block">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-7 col-lg-7 col-md-6 col-sm-12 col-12">
+                    <h3 class="text-white mb0 mt10">Would you like to connect with us</h3>
+                </div>
+                <div class="col-xl-5 col-lg-5 col-md-6 col-sm-12 col-12 text-right">
+                    <div class="social-icons">
+                        <a href="#" class="icon-square"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#" class="icon-square"><i class="fab fa-twitter"></i> </a>
+                        <a href="#" class="icon-square"><i class="fab fa-google-plus-g"></i></a>
+                        <a href="#" class="icon-square"><i class="fab fa-instagram"></i></a>
+                        <a href="#" class="icon-square"><i class="fab fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.social-media-section -->
+     <!-- footer-section -->
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
+                    <!-- footer-widget -->
+                    <div class="footer-widget">
+                        <a href="#"><img src="images/logo.png" alt="" class="mb20"></a>
+                        <p class="mb10">Bring the vision of brides and grooms to life by planning, organizing and overseeing all aspects of the wedding ceremony, reception and, often, the honeymoon.</p>
+                    </div>
+                </div>
+                <!-- /.footer-widget -->
+                <!-- footer-widget -->
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-12">
+                    <div class="footer-widget">
+                        <h3 class="widget-title">
+                            Contact Address
+                        </h3>
+                        <p>Ja-ela, Colombo,
+                            <br> Sri Lanka</p>
+                        <p class="mb0 text-default">+9477 850 8066</p>
+                        <p class="mb0 text-default">silverstar@silverstar.com</p>
+                    </div>
+                </div>
+                <!-- /.footer-widget -->
+                <!-- footer-widget -->
+                <div class="col-xl-2 col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="footer-widget">
+                        <h3 class="widget-title">
+                            About Company
+                        </h3>
+                        <ul class="listnone">
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Contact us</a></li>
+                            <li><a href="#">Faq</a></li>
+                            <li><a href="#">Pricing Plan</a></li>
+                            <li><a href="#">Meet The Team</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- /.footer-widget -->
+                <!-- /.footer-widget -->
+                <div class="col-xl-3 col-lg-3 col-md-12 col-sm-6 col-12">
+                    <div class="footer-widget">
+                        <h3 class="widget-title">
+                            List you Business
+                        </h3>
+                        <p>Are you vendor ? List your venue and service get more from listing business.</p>
+                        <a href="vendor-form.html" class="btn btn-default">List your Business</a>
+                    </div>
+                </div>
+                <!-- /.footer-widget -->
+            </div>
+        </div>
+    </div>
+    <!-- tiny-footer-section -->
+    <div class="tiny-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-right">
+                    <p>© 2018 Silver Star. All Rights Reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.tiny-footer-section -->
+    <a href="javascript:" id="return-to-top"><i class="fa fa-angle-up"></i></a>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/menumaker.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/fastclick.js"></script>
+    <script src="js/custom-script.js"></script>
+    <script src="js/return-to-top.js"></script>
+    <script>
+    // When the user scrolls down 20px from the top of the document, slide down the navbar
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            document.getElementById("header").style.top = "0";
+        } else {
+            document.getElementById("header").style.top = "-90px";
+        }
+    }
+</script>
+</body>
+
+</html>
